25d237790b3d02b3b88bae3159160c108a23bca3
Emilyn Fernandes <143656727+Emilynsfer@users.noreply.github.com>
2024-10-04 20:28:23 -0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+//Declaração de variáveis. 
+//A primeira variável (string) é para o nome fictício do personagem e a segunda variável (number) é para armazenar o valor total da experiência do personagem.
+//Caso o valor da segunda variável não seja um valor numérico, será apresentada uma mensagem de "operação inválida" ao final da compilação do código. 
 let nomeHeroi = "Lulu"
 let pontosDeExperiencia = "10000"
 
+//Verifica em qual nível está o personagem de acordo com as condições apresentadas e o valor numérico armazenado na variável pontosDeExperiencia.
+//Após a verificação, se os valores das variáveis forem validos, será apresentado uma mensagem de saída com o nome e o nivel de classificação do personagem. 
 if (pontosDeExperiencia < 1000) {
     console.log("O herói de nome " + nomeHeroi + " está no nível Ferro")
 } else if (pontosDeExperiencia >= 1001 && pontosDeExperiencia <= 2000) {
@@ -27,5 +32,7 @@ if (pontosDeExperiencia < 1000) {
 } else {
     console.log("está operação não pode ser realizada")
 }
-    
+
+//fim do código
+
     
