487c0c2fc58dc002e357f13ae47e688b9276c459
Emilyn Fernandes <143656727+Emilynsfer@users.noreply.github.com>
2024-10-04 15:30:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let nomeHeroi = "Lulu"
+let pontosDeExperiencia = "b"
+
+if (pontosDeExperiencia < 1000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ferro")
+} else if (pontosDeExperiencia >= 1001 && pontosDeExperiencia <= 2000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Bronze")
+
+} else if (pontosDeExperiencia >= 2001 && pontosDeExperiencia <= 5000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Prata") 
+
+} else if (pontosDeExperiencia >= 5001 && pontosDeExperiencia <= 7000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ouro")
+
+} else if (pontosDeExperiencia >= 7001 && pontosDeExperiencia <= 8000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Platina")
+
+} else if (pontosDeExperiencia >= 8001 && pontosDeExperiencia <= 9000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ascendente")
+
+} else if (pontosDeExperiencia >= 9001 && pontosDeExperiencia <= 10000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Imortal")
+
+} else if (pontosDeExperiencia >= 10001) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Radiante")
+
+} else {
+    console.log("está operação não pode ser realizada")
+}
+    
+    
